f5916c62ad05f37976dca3f24bc9abc921818f81
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-16 18:22:28 +0530

    Answer Domain Model

--- StackOverflowClone.DomainModels/Answer.cs
+++ StackOverflowClone.DomainModels/Answer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace StackOverflowClone.DomainModels
 {
     public class Answer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AnswerID { get; set; }
+        public string AnswerText { get; set; }
+
+        public DateTime AnswerDate { get; set; } = DateTime.Now;
+        public int UserID { get; set; }
+
+        public int QuestionID { get; set; }
+
+        public int VotesCount { get;set; }
+
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+        public virtual List<Vote> Votes { get; set; }
     }
 }
--- StackOverflowClone.DomainModels/Question.cs
+++ StackOverflowClone.DomainModels/Question.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StackOverflowClone.DomainModels
 {
     public class Question
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int QuestionID { get; set; }
         public string QuestionName { get; set; }
         public DateTime QuestionDateAndTime { get; set; }
