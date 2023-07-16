1e250b386b1074db323acad957952f28026a63df
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-16 20:28:32 +0530

    Vote Domain Model

--- StackOverflowClone.DomainModels/Vote.cs
+++ StackOverflowClone.DomainModels/Vote.cs
@@ -8,5 +8,10 @@ namespace StackOverflowClone.DomainModels
 {
     public class Vote
     {
+        public int VoteID { get; set; }
+        public int UserID { get; set; }
+        public int AnswerID { get; set; }
+        public int VoteValue { get; set; }
+
     }
 }
