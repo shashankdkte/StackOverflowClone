42e57f135dc6f27dc5f388ce2492fd573225ea0f
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-17 17:05:41 +0530

    EditUserDetails ViewModel

---
+++ StackOverflowClone.ViewModels/EditUserDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowClone.ViewModels
+{
+    public class EditUserDetailsViewModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z ]*$")]
+        public string Name { get;set; }
+        [Required]
+        public string MobileNumber { get; set; }
+    }
+}
