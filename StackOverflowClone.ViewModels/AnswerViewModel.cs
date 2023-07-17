07232cdaccdeadece5569c2b7276bcfe0adb6ffb
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-17 18:16:15 +0530

    Answer ViewModel

--- StackOverflowClone.ViewModels/AnswerViewModel.cs
+++ StackOverflowClone.ViewModels/AnswerViewModel.cs
@@ -8,5 +8,14 @@ namespace StackOverflowClone.ViewModels
 {
     public class AnswerViewModel
     {
+        public int AnswerID { get; set; }
+        public string AnswerText { get; set; }
+        public DateTime AnswerDate { get; set; }
+        public int UserID { get; set; }
+        public int QuestionID { get; set; }
+        public int VotesCount { get; set; }
+        public virtual UserViewModel User { get; set; }
+        public virtual List<VoteViewModel> Votes { get; set; }
+        public int CurrentUserVoteType { get; set; }
     }
 }
