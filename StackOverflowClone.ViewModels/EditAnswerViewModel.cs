b9742d3eb7f2aa2e89a8521926bbbab1b116887c
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-17 18:19:18 +0530

    EditAnswer View model

---
+++ StackOverflowClone.ViewModels/EditAnswerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowClone.ViewModels
+{
+    public class EditAnswerViewModel
+    {
+        [Required]
+        public int AnswerID { get; set; }
+        [Required]
+        public string AnswerText { get; set; }
+        [Required]
+        public DateTime AnswerDate { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public int QuestionID { get; set; }
+        [Required]
+        public int VotesCount { get; set; }
+        [Required]
+        public virtual QuestionViewModel Question { get; set; }
+    }
+}
