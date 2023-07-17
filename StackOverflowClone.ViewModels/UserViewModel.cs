8ba2b7b4a0b8b4fb6b0f3a0199b3276f6a263443
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-17 15:24:53 +0530

    User ViewModel

---
+++ StackOverflowClone.ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowClone.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }    
+        public string Name { get; set; }
+        public string MobileNumber { get; set; }    
+    public string PhoneNumber { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
