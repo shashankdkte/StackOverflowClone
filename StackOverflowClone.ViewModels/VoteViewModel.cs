37dc4bdbb9a4aa41d2385d63b63f72067e27795a
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-17 18:21:30 +0530

    Vote ViewModel

--- StackOverflowClone.ViewModels/VoteViewModel.cs
+++ StackOverflowClone.ViewModels/VoteViewModel.cs
@@ -8,5 +8,9 @@ namespace StackOverflowClone.ViewModels
 {
     public class VoteViewModel
     {
+        public int VoteID { get; set; }
+        public int UserID { get; set; }
+        public int AnswerID { get; set; }
+        public int VoteValue { get; set; }
     }
 }
