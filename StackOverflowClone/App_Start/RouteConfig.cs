cf9b2e206734afca8c56b2e56d6b4e77fde7324f
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-20 13:09:37 +0530

    Search Questions

--- StackOverflowClone/App_Start/RouteConfig.cs
+++ StackOverflowClone/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace StackOverflowClone
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- StackOverflowClone/Controllers/HomeController.cs
+++ StackOverflowClone/Controllers/HomeController.cs
@@ -41,5 +41,15 @@ namespace StackOverflowClone.Controllers
             List<QuestionViewModel> questions = this.questionService.GetQuestions();
             return View(questions);
         }
+
+        public ActionResult Search(string str)
+        {
+            List<QuestionViewModel> questions = this.questionService.GetQuestions().
+                Where(temp => temp.QuestionName.ToLower().Contains(str.ToLower())
+                 ||
+                 temp.Category.CategoryName.ToLower().Contains(str.ToLower())).ToList();
+            ViewBag.str = str;
+            return View(questions);
+        }
     }
 }
